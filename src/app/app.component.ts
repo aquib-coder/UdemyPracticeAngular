226f888e9834b8fdeef8694a1895c8962875fbfe
aquib javed <Aquib>
2022-03-21 00:39:58 +0530

    Implemented Local references

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UdemyPractice';
-  serverElements =[{type:'server',content:"testContent",name:'testName'}] ;
+  serverElements:any =[]  ;
 
   OnServerAdded(serverData:{serverName:string,serverContent:string}){
     this.serverElements.push({
