8342ee773559087701faf77d6fa22347f35e50ca
Aquib Javed <atif9087@gmail.com>
2022-03-23 15:43:42 +0530

    5 modules completed

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ContentChild, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css']
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent implements OnInit,AfterViewInit,AfterContentInit {
+  @ViewChild('msg') msg:ElementRef;
+  @ContentChild('msg1') msg1:ElementRef;
 n:number=0;
 message:string='';
   constructor() { }
 
   ngOnInit(): void {
+    console.log('ngOnInit called');
+    console.log('counter:'+this.msg1.nativeElement.textContent);
+  }
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit called');
+    console.log('counter:'+this.msg1.nativeElement.textContent);
+  }
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit called');
+    console.log('counter:'+this.msg1.nativeElement.textContent);
   }
 IncreaseBy1(){
 this.n=this.n+1;
